38042de260e935a01c369d1c99524a1a46293fe6
Nedotyopa <65170085+Nedotyopa@users.noreply.github.com>
2022-03-16 23:41:49 +0300

    Create CommentServiceImplTest.java

---
+++ sd4parent/test/java/net/springboot/service/CommentServiceImplTest.java
@@ -0,0 +1,33 @@
+package net.javaguides.springboot.springsecurity.service;
+
+import junit.framework.TestCase;
+import net.javaguides.springboot.springsecurity.converter.CommentConverter;
+import net.javaguides.springboot.springsecurity.model.entity.Comment;
+import net.javaguides.springboot.springsecurity.model.response.CommentResponse;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class CommentServiceImplTest extends TestCase {
+
+    @Autowired
+    CommentConverter commentConverter;
+
+    @Test
+    void checkConverterComment() {
+        CommentResponse commentDto = commentConverter.convertToToResponse(getComment());
+        assertNotNull(commentDto.getMessage(), getComment().getMessage());
+        assertNotNull(commentDto.getEmail(), getComment().getEmail());
+    }
+
+    Comment getComment() {
+        Comment comment = new Comment();
+        comment.setEmail("ass@gmail.com");
+        comment.setId(3L);
+        comment.setMessage("message");
+        return comment;
+    }
+
+}
